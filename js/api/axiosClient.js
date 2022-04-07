842fc241174eaca2242752626fbdcdfac485915b
Dien Cap <baodien1405@gmail.com>
2022-04-07 15:26:33 +0700

    Render post list and install time library

---
+++ js/api/axiosClient.js
@@ -0,0 +1,56 @@
+import axios from 'axios'
+
+const axiosClient = axios.create({
+  baseURL: 'https://js-post-api.herokuapp.com/api',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+// Add a request interceptor
+axiosClient.interceptors.request.use(
+  function (config) {
+    // Do something before request is sent
+
+    // Attach token to request if exists
+    const accessToken = localStorage.getItem('access_token')
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`
+    }
+
+    return config
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error)
+  }
+)
+
+// Add a response interceptor
+axiosClient.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    // Transform data for all response
+    return response.data
+  },
+  function (error) {
+    console.log('axiosClient - response message', error.response)
+    if (!error.response) throw new Error('Network error. Please try ahain later.')
+
+    // redirect to login if not login
+    if (error.response.status === 401) {
+      // clear token, logout
+      // ...
+      window.location.assign('/login.html')
+      return
+    }
+
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    // return Promise.reject(error)
+    throw new Error(error)
+  }
+)
+
+export default axiosClient
